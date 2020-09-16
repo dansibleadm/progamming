d9a9f0b261301e1d148dad7d5674788c20426645
Daniel Bannykh <daniil.bannykh@huawei.com>
2020-09-16 19:38:02 +0300

    Small fix

--- checking_password.py
+++ checking_password.py
@@ -1,10 +1,15 @@
-password = input('Enter the pasword: ')
+from hashlib import md5
+from base64 import b64encode
+
+from getpass import getpass
+
+password = getpass('Enter the pasword: ')
 symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '\'', '\"', ',', '.']
 level = ''
 
 if len(password) >= 12:
-	for sym in symbols:
-		if sym in password:
+	for c in password:
+		if c in symbols:
 			level = 'Too simple'
 			break
 	for c in password:
@@ -15,7 +20,9 @@ if len(password) >= 12:
 		if c.isupper():
 			level = 'Hard'
 			break
-else
+else:
 	level = 'Too short'
+	exit()
 
 print(level)
+print('Your password {}'.format(md5(b64encode(password.encode())).hexdigest()))
