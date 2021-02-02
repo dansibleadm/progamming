be604ea6ad502a998ac4023191893cafe3b399c0
Daniel Bannykh <daniil.bannykh@huawei.com>
2021-02-02 19:17:43 +0300

    fix

--- Exam/Task-1.py
+++ Exam/Task-1.py
@@ -8,7 +8,7 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 logging.info("Init")
 
 try:
-	arr = str(input("Enter first list (example: 1,2,3)>")).split(',')
+	arr = str(input("Enter original list (example: 1,2,3)>")).split(',')
 	brr = str(input("Enter second list>")).split(',')
 	logging.info(f"{arr} -> len({len(arr)})")
 	logging.info(f"{brr} -> len({len(brr)})")
@@ -20,10 +20,12 @@ try:
 	for key, value in arr_count.items():
 		if key not in brr:
 			for i in range(arr_count[key]):
+				brr.append(key)
 				found.append(key)
+			brr_count = dict(Counter(brr))
 		else:
 			if value > brr_count[key]:
-				for i in range(arr_count[key]-value):
+				for i in range(value-brr_count[key]):
 					found.append(key)
 
 	logging.info(f"Lost items: {found}")
